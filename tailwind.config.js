bc67642c0d440ab5d017dbdf732d7e204578164b
Abdullahbadres <abdullahbadres@gmail.com>
2025-04-27 07:41:44 +0700

    kedua

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
       extend: {},
+      screens: {
+        xs: '320px',     // Mobile kecil
+        sm: '768px',     // Tablet
+        md: '1152px',    // Desktop kecil
+        lg: '1440px',    // Desktop medium
+        xl: '1920px',    // Desktop besar
+      },
     },
     plugins: [
       require('@tailwindcss/typography'),
